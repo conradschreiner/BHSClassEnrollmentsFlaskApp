33094a987b2ecb3037122e421154b40874319160
conradschreiner <conradschrei@gmail.com>
2025-02-25 22:01:29 -0500

    added select_all_gradelevels.sql to store the gradelevels query, configred the template for gradelevels with our html, fixed hrefs on both pages for the homepage. it needs to use / instead of index.j2 because that is the root route. I fixed it on the students page as well.

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ from flask import Flask, render_template, json, redirect
 from flask_mysqldb import MySQL
 from flask import request
 from read_sql_file import read_sql_file
-import logging
-import time
 
 app = Flask(__name__)
 
@@ -25,12 +23,26 @@ def root():
 def students():
     """Route CRUD methods to the Students Entity Page"""
 
+    # load query from file and store as string variable
     students_query = read_sql_file(r"database/sql_storage/select_all_students.sql")
 
+    # run query and generate jinja template
     cursor = db.execute_query(db_connection=db_connection, query=students_query)
     results = cursor.fetchall()
-    logging.info(f"Results: {results}")
     return render_template("students.j2", students=results)
+
+@app.route("/gradelevels.j2", methods=["POST", "GET"])
+def gradelevels():
+    """Route CRUD methods to the GradeLevel Entity Page"""
+
+    # load query from file and store as string variable
+    gradelevels_query = read_sql_file(r"database/sql_storage/select_all_gradelevels.sql")
+
+    # run query and generate jinja template
+    cursor = db.execute_query(db_connection=db_connection, query=gradelevels_query)
+    results = cursor.fetchall()
+    return render_template("gradelevels.j2", gradelevels=results)
+
 
 # Listener
 if __name__ == "__main__":
---
+++ database/sql_storage/select_all_gradelevels.sql
@@ -0,0 +1,3 @@
+SELECT gl.gradeLevelID, gl.gradeName as "Grade Name", gl.gradeNumber as "Grade Number"
+FROM `GradeLevels` gl
+ORDER BY gl.gradeLevelID;
