dd6d78682c58f7f03a39be5b19d01dd24058bb7c
conradschreiner <conradschrei@gmail.com>
2025-02-25 23:23:31 -0500

    added Enrollments query and webpage. Also cleaned up some minor issues on the templates that resulted from find and replace all when copying over.

---
+++ database/sql_storage/select_all_enrollments.sql
@@ -0,0 +1,11 @@
+SELECT e.enrollmentID, e.enrolledDate as "Date Student Enrolled",
+       CONCAT(s.fName, ' ', s.lName) AS "Enrolled Student Name",
+       cs.period AS "Period", cs.classroom AS "Classroom",
+       CONCAT(YEAR(cs.startDate), '-', YEAR(cs.endDate))AS "School Year",
+       c.name AS "Course Name", CONCAT(t.fName, ' ', t.lName) AS "ClassSection's Teacher Name"
+FROM `Enrollments` e
+INNER JOIN `Students` s ON e.studentID = s.studentID
+INNER JOIN `ClassSections` cs ON e.classSectionID = cs.classSectionID
+INNER JOIN `Courses` c ON c.courseID = cs.courseID
+LEFT JOIN `Teachers` t ON cs.teacherID = t.teacherID -- to account for NULL and/or "NULLed" Teachers
+ORDER BY e.enrollmentID;
