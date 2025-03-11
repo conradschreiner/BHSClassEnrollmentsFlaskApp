06678b1bec32e3062076aef68ade7aa2e06aece4
conradschreiner <conradschrei@gmail.com>
2025-03-10 22:52:07 -0400

    added GradeLevel.gradeName column to both ClassSections table query and the Enrollments table query

--- database/sql_storage/select_all_classsections.sql
+++ database/sql_storage/select_all_classsections.sql
@@ -1,8 +1,9 @@
 SELECT cs.classSectionID, cs.startDate AS "Class Start Date", cs.endDate AS "Class End Date",
        CONCAT(YEAR(cs.startDate), '-', YEAR(cs.endDate))AS "School Year",
-       cs.period AS "Period", cs.classroom as "Classroom", c.name as "Course Name",
+       cs.period AS "Period", cs.classroom as "Classroom", c.name as "Course Name", gl.gradeName as "Course Grade Name",
        CONCAT(t.fName, ' ', t.lName) AS "Teacher Name" -- including to better understand the NULLable foreign key
 FROM `ClassSections` cs
 INNER JOIN `Courses` c on cs.courseID = c.courseID
+INNER JOIN `GradeLevels` gl on c.gradeLevelID = gl.gradeLevelID
 LEFT JOIN `Teachers` t on cs.teacherID = t.teacherID -- to account for NULL and/or "NULLed" Teachers
 ORDER BY cs.classSectionID;
