579fa33c86286aa254c816830212a1837d53d96d
conradschreiner <conradschrei@gmail.com>
2025-03-11 22:33:07 -0400

    refactored enrollment dropdown queries to a cleaner presentation

--- database/sql_storage/select_student_names.sql
+++ database/sql_storage/select_student_names.sql
@@ -1,3 +1,3 @@
-SELECT s.studentID, CONCAT('Full Name - ', CONCAT(s.fName, ' ', s.lName), ' | Grade Level - ', gl.gradeName) as student_record
+SELECT s.studentID, CONCAT(CONCAT(s.fName, ' ', s.lName), ', ', gl.gradeName) as student_record
 FROM `Students` s
 INNER JOIN `GradeLevels` gl on s.gradeLevelID = gl.gradeLevelID;
