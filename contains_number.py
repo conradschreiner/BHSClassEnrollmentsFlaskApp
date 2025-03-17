b34ca90418d9c51a8dd9ead76fc89da78defc96f
conradschreiner <schrecon@oregonstate.edu>
2025-03-17 10:21:49 -0700

    added more documentation

--- contains_number.py
+++ contains_number.py
@@ -1,3 +1,6 @@
+# Written by Conrad by referencing the below listed geeks for geeks article. It is used on the backend for checking if 
+# name field input sent by a user contains a numerical character or not. 
+
 def contains_number(s):
     """Check if the string contains any numerical character.
     source: https://www.geeksforgeeks.org/python-check-if-given-string-is-numeric-or-not/"""
--- read_sql_file.py
+++ read_sql_file.py
@@ -1,5 +1,12 @@
+# Writen by Conrad so that SQL queries could be stored in seperate files for organization, and then stored as string variables
+# when needed. It is used in all of the pages READ functions, as well as on all of the dropdowns that Conrad worked on.
+# Utlized Python's built-in open() function and referenced it using the official Python documentation: https://docs.python.org/3/library/functions.html#open.
+
 def read_sql_file(sql_file):
-    """Reads a sql file and returns it as a string"""
+    """Reads a sql file and returns it as a string.
+    Utilized Python's built-in open() function and referenced it using the official Python documentation: 
+    https://docs.python.org/3/library/functions.html#open.
+    """
 
     with open(sql_file) as f:
         return f.read()
